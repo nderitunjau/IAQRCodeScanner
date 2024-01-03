a05271692a4b1e41d5e222fa805bda05e40c52bd
nderitu_njau <nderitunjau@gmail.com>
2024-01-03 08:34:17 +0300

    Update MainActivity.kt

--- app/src/main/java/com/gdff/iaqrcodescanner/MainActivity.kt
+++ app/src/main/java/com/gdff/iaqrcodescanner/MainActivity.kt
@@ -33,9 +33,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         }
+        //This shows all the data in one text.
     private lateinit var binding:ActivityMainBinding
     private fun setResult(string: String){
         binding.textResult.text=string
+    }
+    //This shows code parsed in small bits
+    
+     private fun setResult(string: String){
+        val parts = string.split("~") // Split the string by ~
+        if (parts.size > 2) {
+            val specificPart = parts[2] // Accessing the third element (index 2)
+            binding.seg3.text = specificPart // Display the specific part
+        } else {
+            // Handle the case where there might not be enough parts
+            binding.seg3.text = "No valid data found"
+           // binding.seg1.text = parts[0]
+           // binding.seg2.text = parts[1]
+           // binding.seg3.text = parts[2]
+        }
+        //binding.textResult.text=string
     }
 
     private fun popCamera() {
