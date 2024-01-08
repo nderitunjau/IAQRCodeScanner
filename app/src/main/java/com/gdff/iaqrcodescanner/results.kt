0f02588564b7d0988720f6ee81a7b75fe2580bc6
nderitu_njau <nderitunjau@gmail.com>
2024-01-08 09:26:03 +0300

    Update results.kt
    Shows results KRA Invoice results in a webview activity

--- app/src/main/java/com/gdff/iaqrcodescanner/results.kt
+++ app/src/main/java/com/gdff/iaqrcodescanner/results.kt
@@ -2,10 +2,18 @@ package com.gdff.iaqrcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import com.gdff.iaqrcodescanner.databinding.ActivityMainBinding
 
 class results : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+        val webView = findViewById<WebView>(R.id.webView)
+        // Retrieve the URL from the Intent extras or wherever you're storing it
+        val url = intent.getStringExtra("URL_KEY") ?: ""
+        // Load the URL in the WebView
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(url)
     }
 }
